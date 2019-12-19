90770d6bbd31a9a411a30449ee2ab2c650e90cab
Kevin Sallee <kevin.sallee@gpltech.com>
2019-12-18 22:45:39 -0600

    changed the code for more clarity

--- JoyStick.gd
+++ JoyStick.gd
@@ -1,95 +1,49 @@
-#Author: Rodrigo Torres
-#Version: 1.0.5
-#For Godot 3.x
+# Module used to control a joystick made of two circles.
+# The position of the joystick is fixed if controlled by the mouse.
+# It's under the finger if controlled by touch
 extends Node2D
-
 signal move
 
-onready var bigCircle = $BigCircle
-onready var smallCircle = $SmallCircle
-
-var resetPosCircle
-var pressed = 0
-var halfBigCircleSize
-var thereIsEventInput = false
-var vectorToEmit
-var bigCircPos
-var distance
-
-func _ready():
-	halfBigCircleSize = bigCircle.texture.get_size().x / 2
-	updateCachedCirclesPositions()
-
+onready var big_circle = get_node("BigCircle")
+onready var small_circle = get_node("SmallCircle")
+onready var big_circle_radius = big_circle.texture.get_size().x / 2	
+var input_ongoing = false
+	
+func ready():
+	# need to test
+	if OS.has_touchscreen_ui_hint():
+		self.visible=false
 func _input(event):
-	if event is InputEventKey:
-		return
-	
-	_on_Pressed(event)
-	
-	if event is InputEventScreenTouch:
-		toggleVisible(event.pressed)
-		setSelfPosition(event.position)
-		updateCachedCirclesPositions()
+	# If it's a touch event (pressed, released)
+	if event is InputEventScreenTouch or event is InputEventMouseButton:
 		if not event.pressed:
-			emit_signal_move(Vector2(0, 0))
+			# Stop moving.
+			emit_signal('move', Vector2())
+			# Reset joystick position
+			small_circle.global_position = big_circle.global_position
+			# Stop tracking position 
+			input_ongoing = false
+			# If touch screen, hide the control
+			if event is InputEventScreenTouch:
+				self.visible = false
 			return
-
-	if getIsDrag(event) and pressed == 1:
-		var dirBigCir_dirEnvt = event.position - bigCircle.get_global_position()
-		distance = getDistance(dirBigCir_dirEnvt.x, 0, dirBigCir_dirEnvt.y, 0)
-		if distance > halfBigCircleSize:
-			smallCircle.set_position(dirBigCir_dirEnvt.normalized() * halfBigCircleSize)
 		else:
-			smallCircle.set_global_position(event.position)
-			
-	if isReleased(event):
-		pressed = 0
-		smallCircle.set_global_position(resetPosCircle)
-
-	vectorToEmit = smallCircle.get_position()
-	thereIsEventInput = true if event else false
+			# Start tracking position
+			input_ongoing = true
+			# If touch screen, show control under the finger
+			if event is InputEventScreenTouch:
+				self.visible = true
+				self.position = event.position
+			return
+	# Move event: set joystick position
+	if (event is InputEventMouseMotion or event is InputEventScreenDrag) and input_ongoing:
+		var motion_vector = event.position - big_circle.get_global_position()
+		if motion_vector.length() > big_circle_radius:
+			small_circle.set_position(motion_vector.normalized() * big_circle_radius)
+		else:
+			small_circle.set_global_position(event.position)
 
 func _process(delta):
-		#normalized() reduces the magnitude of the vector to 1 while maintaining the direction
-		if thereIsEventInput and vectorToEmit:
-			emit_signal_move(vectorToEmit / halfBigCircleSize)
-
-func toggleVisible(value):
-	self.visible = value
-
-func setSelfPosition(position):
-	self.position = position
-	smallCircle.set_global_position(position)
-
-func updateCachedCirclesPositions():
-	resetPosCircle = smallCircle.get_global_position()
-	bigCircPos = bigCircle.get_global_position()
-
-func easing(t):
-	return t*t*t
-
-func emit_signal_move(value):
-	emit_signal('move', easing(value))
-
-func getIsDrag(event):
-	if event is InputEventMouseMotion or event is InputEventScreenDrag:
-		return true
-
-func _on_Pressed(event):
-	var eventPos = event.get_position()
-
-	if not eventPos or not eventPos.x or not eventPos.y:
-		return
-	
-	var distCirc_eventPos = getDistance(eventPos.x, bigCircPos.x, bigCircPos.y, eventPos.y)
-	
-	if event is InputEventMouseButton or event is InputEventScreenTouch:
-		if distCirc_eventPos <= halfBigCircleSize:
-			pressed = 1
-
-func isReleased(event):
-	if event is InputEventScreenTouch or event is InputEventMouseButton:
-		return !event.pressed
-
-func getDistance(x1, x2, y1, y2):
-	return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
+	if input_ongoing:
+		var vector = small_circle.position / big_circle_radius
+		emit_signal("move", vector*vector*vector)
